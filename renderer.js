bdf0bcc4674cb7e316dfc2272a8402ab7cba36fa
Titoine ⌚️🇫🇷🇨🇦 <123580047+titoinemb@users.noreply.github.com>
2024-12-23 09:49:57 -0500

    Add files via upload

--- renderer.js
+++ renderer.js
@@ -1,19 +1,25 @@
-const { exec } = require('child_process');
+const { exec, } = require('child_process');
 const path = require('path');
 const NodeID3 = require('node-id3');
-const axios = require('axios')
+const fs = require('fs');
+const iconv = require('iconv-lite');
+const axios = require('axios');
 
-function dowload(url, callback) {
-  return exec(__dirname + `\\bin\\yt-dlp.exe -x --audio-format mp3 -o "${__dirname}\\dwn-tmp\\%(title)s.%(ext)s" "${url}"`, (err, stdout, stderr) => {
+function download(url, callback) {
+  let command = `${__dirname}\\bin\\yt-dlp.exe -x --audio-format mp3 -o "${__dirname}\\dwn-tmp\\%(title)s.%(ext)s" "${url}"`;
+  
+  exec(command, { encoding: 'buffer' }, (err, stdout, stderr) => {
     if (err) {
-      return callback(err)
-    };
-    return callback(null, stdout);
+      console.error('Erreur:', iconv.decode(stderr, 'utf-8'));
+      return callback(err);
+    }
+    return callback(null, iconv.decode(stdout, 'utf-8'));
   });
-};
+}
+
 
 function convert(url, callback) {
-  return exec(__dirname + `\\bin\\ffmpeg.exe -i "${url}" -codec:a libmp3lame -qscale:a 0 "${__dirname}\\download\\${url.split('\\').pop().replace('.mp3', '').trim()}.mp3"`, (err, stdout, stderr) => {
+  return exec(__dirname + `\\bin\\ffmpeg.exe -i "${url}" -codec:a libmp3lame -qscale:a 0 "${__dirname}\\download\\${url.split('\\').pop().replace('.mp3', '').trim()}.mp3"`, 'utf8', (err, stdout, stderr) => {
     if (err) {
       console.error(`err : ${err}`);
       return callback(err)
@@ -72,32 +78,43 @@ function deleteFile(url, callback) {
 
 document.getElementById('menu1-button').addEventListener('click', () => {
   document.getElementById("load").style.display = "block";
-  let list = document.getElementById('menu1-text').value.split('\n');
+  const list = document.getElementById('menu1-text').value.split('\n');
 
-  for (i = 0; i <= list.length - 1; i++) {
-    if (/^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/.+$/.test(list[i])) {
+  var ll = list.length -1;
+
+  for(i = 0; i <= ll; i++) {
+    if(/^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/.+$/.test(list[i])) {
       let url = list[i];
-      dowload(list[i], (err, result) => {
+      download(url, (err, result) => {
         if (!err) {
-          let extractedFile = (result.split('[ExtractAudio] Destination: ')[1] || '').split('\n')[0].trim();
-          convert(extractedFile, (err) => {
-            if (!err) {
-              deleteFile(extractedFile, (err) => {
-                if (!err) {
-                  // metadata(__dirname+"\\download\\"+extractedFile.split('\\').pop().replace('.mp3', '').trim()+".mp3", url);
-                  if(i = list.length-1) {
-                    document.getElementById("load").style.display = "none";
-                    document.getElementById("file").style.display = "block";
-                  };
-                };
-              });
+          axios.get('https://www.youtube.com/oembed?format=json&url='+url) 
+          .then(response => {
+            let url_file = `${__dirname}\\dwn-tmp\\${response.data.title}.mp3`;
+            
+            if(fs.existsSync(`${__dirname}\\download\\${response.data.title}.mp3`)) {
+              deleteFile(`${__dirname}\\download\\${response.data.title}.mp3`);
             };
-          });
+
+            convert(url_file, (err) => {
+              if (!err) {
+                deleteFile(url_file, () => {
+                  // metadata(__dirname+"\\download\\"+extractedFile.split('\\').pop().replace('.mp3', '').trim()+".mp3", url);
+                });
+              };
+            });
+          }) ;
         };
       });
+    } else {
+      if(ll == 0) {
+        document.getElementById("load").style.display = "none";
+      };
+    };
+
+    if(i == list.length) {
+      document.getElementById("load").style.display = "none";
     };
   };
-
 });
 
 document.getElementById('button-folder').addEventListener('click', () => {
